c5578a0de4f2f1e916f27a01d11de335c80b4d31
Hue <tinyman2308@gmail.com>
2021-12-13 15:39:14 +0700

    change package files

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "Reflection"
+    name: "Reflection",
+    products: [
+        .library(name: "Reflection", targets: ["Reflection"]),
+    ],
+    targets: [
+        .target(
+            name: "Reflection", 
+            dependencies: []
+        )
+    ],
 )
